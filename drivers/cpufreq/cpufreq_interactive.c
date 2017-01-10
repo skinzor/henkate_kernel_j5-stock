005463f92e9e958f726a83eadf61f57c54ce14d6
skinzor <robertpopescu95@yahoo.com>
2017-01-10 15:35:31 +0200

    cpufreq:  Add cpufreq_notify_utilization

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -473,6 +473,8 @@ static void cpufreq_interactive_timer(unsigned long data)
 	cpu_load = loadadjfreq / pcpu->policy->cur;
 	boosted = tunables->boost_val || now < tunables->boostpulse_endtime;
 
+        cpufreq_notify_utilization(pcpu->policy, cpu_load);
+
 	if (cpu_load >= tunables->go_hispeed_load || boosted) {
 		if (pcpu->policy->cur < tunables->hispeed_freq) {
 			new_freq = tunables->hispeed_freq;
