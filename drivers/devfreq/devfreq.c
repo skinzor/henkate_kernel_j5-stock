edaf9d41f69d5834052456b5135fc6423d4c422f
skinzor <robertpopescu95@yahoo.com>
2017-01-17 18:59:54 +0200

    devfreq: enabled High-Priority WorkQueue for devfreq

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -1006,6 +1006,10 @@ static int __init devfreq_init(void)
 	}
 
 	devfreq_wq = create_freezable_workqueue("devfreq_wq");
+		devfreq_wq =
+		alloc_workqueue("devfreq_wq",
+ 			WQ_HIGHPRI | WQ_UNBOUND | WQ_FREEZABLE |
+ 			WQ_MEM_RECLAIM, 0);
 	if (IS_ERR(devfreq_wq)) {
 		class_destroy(devfreq_class);
 		pr_err("%s: couldn't create workqueue\n", __FILE__);
