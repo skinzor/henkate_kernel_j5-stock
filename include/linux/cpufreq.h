f81778873459d3ef72953a1a0b4fc8029259f832
skinzor <robertpopescu95@yahoo.com>
2017-01-22 18:49:58 +0200

    Revert "cpufreq: add Dynamic Interactive governor"
    This reverts commit bae806c199165962a477f50edccc005037117011.
    
    	modified:   drivers/cpufreq/Kconfig
    	modified:   drivers/cpufreq/Makefile
    	deleted:    drivers/cpufreq/cpufreq_dynamic_interactive.c
    	modified:   include/linux/cpufreq.h
    	deleted:    include/trace/events/cpufreq_dynamic_interactive.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -477,9 +477,6 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DYNAMIC_INTERACTIVE)
-extern struct cpufreq_governor cpufreq_gov_dynamic_interactive;
-#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_dynamic_interactive)
 #endif
 
 /*********************************************************************
